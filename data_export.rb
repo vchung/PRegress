74154982783af7dd9159b7f2412e4d04c2626488
vchung <vchung@areteinc.com>
2013-11-01 15:24:38 -0400

    [VYC] Delete and insert settings records in custprop

--- data_export.rb
+++ data_export.rb
@@ -5,13 +5,11 @@ require 'yaml'
 
 java_import 'oracle.jdbc.OracleDriver'
 java_import 'java.sql.DriverManager'
+java_import 'java.sql.ResultSet'
 
 class PRegress
   
-  attr :c_work_dir
-  
   def initialize()
-    @c_work_dir = Dir.pwd
     @o_ser_conn = YAML.load_file('connection.yml')
     @c_ora_url = @o_ser_conn['ora_url']
     @c_ora_user = @o_ser_conn['ora_user']
@@ -22,6 +20,11 @@ class PRegress
     oradriver = OracleDriver.new()
     DriverManager.registerDriver(oradriver)
     @o_conn = DriverManager.get_connection(@c_ora_url, @c_ora_user, @c_ora_pass)
+	
+	# create connection to vfp table
+	s_driver = "sun.jdbc.odbc.JdbcOdbcDriver"
+	s_url = %Q{jdbc:odbc:Driver={#{@o_ser_conn["vfp_driver"]}};SourceType=DBF;SourceDB=#{@o_ser_conn["custom_folder"]};}
+	@o_custom_conn = DriverManager.getConnection(s_url, "", "")
   end
   
   def update_oratable(n_job_id, c_prg_name, t_run_start, t_run_end, n_run_id)
@@ -48,25 +51,60 @@ class PRegress
   end
   
   def run_routine(n_run_id)
-    c_query = %Q{select job_id, prg_name, settings_label from perfjobs order by job_id}
+    c_query = %Q{select job_id, upper(prg_name) prg_name, settings_label, settings_xml, upper(user_name) user_name from perfjobs order by job_id}
     o_stmt = @o_conn.create_statement()
     o_rs = o_stmt.execute_query(c_query)
+	
     while o_rs.next()
+    
       n_job_id = o_rs.get_int("job_id")
       c_prg_name = o_rs.get_string("prg_name")
       c_settings_label = o_rs.get_string("settings_label")
+      c_settings_xml = o_rs.get_string("settings_xml")
+      c_user_name = o_rs.get_string("user_name")
+      
+      setup_settings(c_prg_name, c_user_name, c_settings_xml)
+      
+      # run prevail 
       t_run_start = Time.now()
-	  Dir.chdir(@c_app_folder) do 
-        run_cmd = %x{Prevail8.exe ARETE #{c_prg_name} "SettingLabel='#{c_settings_label}' DoParallel='.T.' ParallelLevel='16'"}
+      Dir.chdir(@c_app_folder) do 
+        run_cmd = %x{Prevail8.exe #{c_user_name} #{c_prg_name} "SettingLabel='#{c_settings_label}'"}
       end
-	  t_run_end = Time.now()
+      t_run_end = Time.now()
 	  
       update_oratable(n_job_id, c_prg_name, t_run_start, t_run_end, n_run_id)
-    end
-    #t_report_end = Time.now
-    #c_report_end = t_report_end.strftime("%m/%d/%Y %H:%M:%S")
-    #update report_end time     
+    end    
   end
+  
+  def setup_settings(c_prg_name, c_user_name, c_settings_xml)
+  
+    # 1. delete all saved setting for routine and user
+    c_del_sql = %Q{delete from custprop where scope = ? and property like ? and userid = ?}
+    o_del_stmt = @o_custom_conn.prepare_statement(c_del_sql)
+    o_del_stmt.set_string(1, c_prg_name)
+    o_del_stmt.set_string(2, "%SETTINGS%")
+    o_del_stmt.set_string(3, c_user_name)
+    n_del_cnt = o_del_stmt.execute_update()
+    @o_custom_conn.commit()
+    
+    puts "n_del_cnt: #{n_del_cnt}"
+ 
+    # 2. insert new settings
+    c_ins_sql = %Q{insert into custprop (scope, property, userid, value, memovalue, lock, type) values (?, ?, ?, ?, ?, ?, ?)}
+    o_ins_stmt = @o_custom_conn.prepare_statement(c_ins_sql)
+    o_ins_stmt.set_string(1, c_prg_name)
+    o_ins_stmt.set_string(2, "SETTINGS.001")
+    o_ins_stmt.set_string(3, c_user_name)
+    o_ins_stmt.set_string(4, "")
+    o_ins_stmt.set_string(5, c_settings_xml)
+    o_ins_stmt.set_string(6, "")
+    o_ins_stmt.set_string(7, "C")
+    n_ins_cnt = o_ins_stmt.execute_update()
+    @o_custom_conn.commit()
+    
+    puts "n_ins_cnt: #{n_ins_cnt}"
+  end
+  
   def get_runlabel()
     c_query = %Q{select seq_run.nextval from dual}
     o_stmt = @o_conn.create_statement()
