99aa93e075e20a8044883c4a68407ee5d9552fac
vchung <vchung@areteinc.com>
2013-11-12 11:54:59 -0500

    [VYC] remove JobFactory, JobRunner methods

--- bin/job_runner.rb
+++ bin/job_runner.rb
@@ -4,97 +4,53 @@ require 'java'
 java_import 'java.lang.Runnable'
 
 
-class JobFactory
-  
-  def self.create_job(o_node, o_logger)
-  
-    o_job_info = o_node.content
-    
-    case
-    when o_job_info.job_type == "S" 
-      o_job = SerialJob.new()
-    when o_job_info.job_type == "P"
-      o_job = ParallelJob.new()
-    else
-      o_job = BasicJob.new()
-    end
-    
-    o_job.node = o_node
-    o_job.logger = o_logger
-    
-    return o_job
-  end
-  
-end
-
-class JobRunner
-
-  def self.run_jobs(o_root_node, o_logger) # rubytree
-    puts "JobRunner.run_jobs..."
-    
-    o_root_node.print_tree()
-    
-    o_job = JobFactory.create_job(o_root_node, o_logger)
-    o_job.run()
-  end
-
-end
-
 module Jobbable
   include Runnable
   
+  attr_accessor :exec_cmd
+  attr_accessor :work_dir
+  attr_accessor :job_type
+  attr_accessor :run_id
+  attr_accessor :job_id
+  attr_accessor :node_id
   attr_accessor :node
-  attr_accessor :logger
+  
+end
+
+module BasicJobable 
+  include Jobbable
   
   def run()
-    o_job_info = @node.content
-    
-    @logger.log_job_start(o_job_info) 
-    
-    run_job()
-    
-    @logger.log_job_end(o_job_info) 
-    
-  end
-  
-  def run_job()
-    # to-be implemented by child class
-  end
-  
-end
-
-class BasicJob 
-  include Jobbable
-  
-  def run_job()
-    o_job_info = @node.content
-    Dir.chdir(o_job_info.work_dir) do 
-      %x{#{o_job_info.exec_cmd}}
+    before_run()
+    Dir.chdir(@work_dir) do 
+      %x{#{@exec_cmd}}
     end  
+    after_run()
   end
   
 end
 
-class SerialJob
+module SerialJobbale
   include Jobbable
   
-  def run_job()
+  def run()
+    before_run()
     @node.children do |o_child_node|
-      o_job = JobFactory.create_job(o_child_node, @logger)
-      o_job.run()
+      run()
     end
+    after_run()
   end
-  
 end
 
-class ParallelJob
+module ParallelJobbale
   include Jobbable
   
-  def run_job()
+  def run()
+    before_run()
     @node.children do |o_child_node|
-      o_job = JobFactory.create_job(o_child_node, @logger)
-      o_job.run()
+      run()
     end
+    after_run()
   end
   
 end
